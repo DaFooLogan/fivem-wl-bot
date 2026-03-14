97f7ef40bc87c53867fa531c0b31a6ce8833fa18
cfx.Logan <cypherpiearson10@gmail.com>
2026-03-14 18:52:18 -0500

    Update message formatting with directional arrows

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.on("messageCreate", async (message) => {
 
 You already have access to **Diamond Roleplay**.
 
-``Use the buttons below to join the server or view important information.`` `
+↘️``Use the buttons below to join the server or view important information.``↙️`
         )
         .setFooter({ text: "Diamond Roleplay" });
 
@@ -87,7 +87,7 @@ You already have access to **Diamond Roleplay**.
 
 Welcome to **Diamond Roleplay**.
 
-``Use the buttons below to join the server or view important information.`` `
+↘️``Use the buttons below to join the server or view important information.``↙️`
       )
       .setFooter({ text: "Diamond Roleplay" });
 
